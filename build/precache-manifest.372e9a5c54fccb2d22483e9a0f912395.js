b99f6f1aabd3608a92ae276f400249f6858b8f8a
Moulik Arora <you@example.com>
2021-08-02 17:06:48 +0530

    adding files

---
+++ build/precache-manifest.372e9a5c54fccb2d22483e9a0f912395.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8224e478da2875fc7041f094e930ff8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16e3e1f9dcbf9b323594",
+    "url": "/static/css/main.96ca4687.chunk.css"
+  },
+  {
+    "revision": "1df55bedf377f348f1da",
+    "url": "/static/js/2.fced7d2a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.fced7d2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16e3e1f9dcbf9b323594",
+    "url": "/static/js/main.6179d25f.chunk.js"
+  },
+  {
+    "revision": "6fc4f2c3fd5ae8d05fd1",
+    "url": "/static/js/runtime-main.4ba1596e.js"
+  }
+]);
